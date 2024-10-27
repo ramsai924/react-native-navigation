473c5b520c34e0c29e144146048bc971bba8e6cc
ramsai <ramsai924@gmail.com>
2024-10-28 00:31:16 +0530

    removed stack navugation files from drwawer nav

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Categories from './screens/Categories';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import CategoryOverView from './screens/CategoryOverView';
-import MealDetails from './screens/MealDetails';
 
 const Stack = createNativeStackNavigator()
 
@@ -13,24 +10,7 @@ export default function App() {
     <>  
       <StatusBar style='auto' />
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerTintColor: 'grey',
-            headerStyle: { backgroundColor: '#e0e2f8' },
-          }}
-        >
-          <Stack.Screen 
-            name='Categories' 
-            component={Categories}
-            // Option one to keep options on screen
-            // options={{
-            //   title: 'All categories',
-            //   // contentStyle: { backgroundColor: 'grey' } //Used for changing screen styles eg: styles
-            // }}
-          />
-          <Stack.Screen name='CategoryOverview' component={CategoryOverView} />
-          <Stack.Screen name='mealDetails' component={MealDetails} />
-        </Stack.Navigator>
+       <View><Text>Hello</Text></View>
       </NavigationContainer>
     </>  
   );
